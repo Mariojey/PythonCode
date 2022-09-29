16dbc27aff31c64720c1d6d5e21c2fb58e8f041b
Mariusz Jacek <marios.yaceg@gmail.com>
2022-09-29 20:37:32 +0200

    Update List

--- List Minimum/main.py
+++ List Minimum/main.py
@@ -1,5 +1,6 @@
-file = open("input.txt").readlines()
-for i in range(1, file[0]):
-    if file[i+1] < file[i]:
-        file[i+1], file[i] = file[i], file[i+1]
-print(list(file))
+file = list(map(int, open("input.txt").readlines()))
+for j in range(1, file[0]):
+    for i in range(1, file[0]):
+        if file[i+1] < file[i]:
+            file[i+1], file[i] = file[i], file[i+1]
+print(list(file)[1:])
